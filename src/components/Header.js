aef31fd25eba596fbd5a83a3a2807ffde6eb37d8
Erik Anderson <itserikanderson@gmail.com>
2018-02-01 11:15:18 -0500

    basic splash functionality

---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import logo from '../images/logo_crop.png'
+
+const Header = (props) => (
+    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
+        <div className="logo">
+            <span className="icon fa-diamond"></span>
+        </div>
+        <div className="content">
+            <div className="inner">
+                <h1><img className="vince-logo" src={logo} alt="vincent fable" width="630px"/></h1>
+                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
+                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+            </ul>
+        </nav>
+        <ul className="icons">
+          <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+          <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
+          <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
+          <li><a href="#" className="icon fa-github"><span className="label">GitHub</span></a></li>
+        </ul>
+    </header>
+)
+
+Header.propTypes = {
+    onOpenArticle: React.PropTypes.func,
+    timeout: React.PropTypes.bool
+}
+
+export default Header
